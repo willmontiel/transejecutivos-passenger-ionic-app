74510e565cb7b5d6e074de754d1bc6136ce1ef97
Will Montiel <will.montiel@aol.com>
2019-01-13 12:11:46 -0500

    Recover password

--- src/pages/recover-password/recover-password.ts
+++ src/pages/recover-password/recover-password.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the RecoverPasswordPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { LoadingController, IonicPage, NavController, NavParams } from 'ionic-angular';
+//Models
+import { User } from '../../models/user';
+//Providers
+import { AuthProvider } from '../../providers/auth/auth';
+import { MiscProvider } from '../../providers/misc/misc';
+import { DbProvider } from '../../providers/db/db';
+import { GlobalProvider } from '../../providers/global/global';
+import { ApiConfigProvider } from '../../providers/api-config/api-config';
+//Pages
+import { LoginPage } from '../../pages/login/login';
 
 @IonicPage()
 @Component({
@@ -14,12 +17,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'recover-password.html',
 })
 export class RecoverPasswordPage {
+  credentials = {username: ''};
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams, 
+    private authProvider: AuthProvider,
+    private miscProvider: MiscProvider,
+    private globalProvider: GlobalProvider,
+    public loadingCtrl: LoadingController,
+    private dbProvider: DbProvider) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad RecoverPasswordPage');
+    
+  }
+
+  public recoverPassword() {
+    let loading = this.loadingCtrl.create({
+      content: 'Cargando'
+    });
+    loading.present();
+
+    this.authProvider.recoverPassword(this.credentials).subscribe(response => {
+        loading.dismiss();
+        this.miscProvider.presentToast(response.message, 'bottom');
+        this.navCtrl.setRoot(LoginPage);
+    },
+    err => {
+        loading.dismiss();
+        let alert = this.miscProvider.createAlert("Error", err, ['Cerrar']);
+        alert.present();
+    });
   }
 
 }
--- src/providers/api-config/api-config.ts
+++ src/providers/api-config/api-config.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-//const URL_BASE: string = "http://app.transportesejecutivos.com";
+//const URL_BASE: string = "https://app.transportesejecutivos.com";
 const URL_BASE: string = "http://dev.transejecutivos.com";
 //const URL_BASE: string = "/api";
 const API_BASE: string = "api/passengerapp";
@@ -10,6 +10,7 @@ const API_V1: string = "v1";
 @Injectable()
 export class ApiConfigProvider {
   login: string = "session/apilogin";
+  recoverPassword: string = "session/apirecoverpassword";
   searchCarType: string = "cartype/searchcartype";
   getCarTypes: string = "cartype/getall";
   getAerolines: string = "aeroline/getall";
@@ -17,12 +18,13 @@ export class ApiConfigProvider {
   getServicesByDate: string = "getservicesbydate";
   requestService: string = "requestservice";
   updateProfile: string = "profile/update";
-
+  
   constructor() {}
 
   get() {
     return {
       login: URL_BASE + "/" + this.login,
+      recoverPassword: URL_BASE + "/" + this.recoverPassword,
       getServicesByDate: URL_BASE + "/" + API_BASE + "/" + API_V1 + "/" + this.getServicesByDate,
       searchCarType: URL_BASE + "/" + this.searchCarType,
       getCarTypes: URL_BASE + "/" + API_GENERAL_BASE + "/" + API_V1 + "/" + this.getCarTypes,
@@ -34,6 +36,6 @@ export class ApiConfigProvider {
   }
 
   getVersion() {
-    return 'Versión 2.0.2';
+    return 'Versión 2.1';
   }
 }
